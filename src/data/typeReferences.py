fb34df11a1dde884673ad331de0efc4dce50dab9
Geismark <geismark@btinternet.com>
2023-07-08 15:23:53 +0100

    added new reference data

---
+++ src/data/typeReferences.py
@@ -0,0 +1,14 @@
+skip_dying_types = [
+    "Shrapnel",
+    "Flare",
+    "Parachutist",
+    "Decoy",
+    "Chaff",
+]
+
+killer_types = [
+    "Weapon",
+    "Missile",
+    "Projectile",
+    "Shell",
+]
